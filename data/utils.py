63d7729d4c37bd2df3867bfaef321ab4706ca024
nyxflower <hxu31@sheffield.ac.uk>
2019-07-16 19:40:49 +0100

    Fix load node feature as torch.sparse type

--- data/utils.py
+++ data/utils.py
@@ -2,9 +2,11 @@ import numpy as np
 import scipy.sparse as sp
 import pickle
 import torch as t
+import torch.sparse as tsp
 from torch_geometric.nn.models.autoencoder import negative_sampling
 
 t.manual_seed(0)
+
 
 def get_drug_index_from_text(code):
     return int(code.split('D')[-1])
@@ -14,7 +16,7 @@ def get_side_effect_index_from_text(code):
     return int(code.split('C')[-1])
 
 
-def load_data(path, dd_et_list, mono=True):
+def load_data_torch(path, dd_et_list, mono=True):
     """
     :param path: WRITE_DATA_PATH in preprocess_data.py
     :param dd_et_list: a list of int - drug indices
@@ -32,7 +34,8 @@ def load_data(path, dd_et_list, mono=True):
     dd_adj_list = []
     sum_adj = sp.csr_matrix((drug_num, drug_num))
     for i in dd_et_list:
-        adj = sp.load_npz(''.join([path, 'sym_adj/drug-sparse-adj/type_', str(i), '.npz']))
+        adj = sp.load_npz(
+            ''.join([path, 'sym_adj/drug-sparse-adj/type_', str(i), '.npz']))
         dd_adj_list.append(adj)
         sum_adj += adj
 
@@ -50,7 +53,8 @@ def load_data(path, dd_et_list, mono=True):
     # drug additional feature
     # ########################################
     if mono:
-        drug_mono_adj = sp.load_npz(path + "node_feature/drug-mono-feature.npz").tocsr()
+        drug_mono_adj = sp.load_npz(
+            path + "node_feature/drug-mono-feature.npz").tocsr()
 
     # ########################################
     # remove isolated drugs
@@ -64,25 +68,47 @@ def load_data(path, dd_et_list, mono=True):
             ind = isolated_drug.pop()
             # remove from d-d adj
             for i in range(len(dd_adj_list)):
-                dd_adj_list[i] = sp.vstack([dd_adj_list[i][:ind, :], dd_adj_list[i][ind + 1:, :]]).tocsr()
-                dd_adj_list[i] = sp.hstack([dd_adj_list[i][:, :ind], dd_adj_list[i][:, ind + 1:]]).tocsr()
+                dd_adj_list[i] = sp.vstack([dd_adj_list[i][:ind, :],
+                                            dd_adj_list[i][ind + 1:,
+                                            :]]).tocsr()
+                dd_adj_list[i] = sp.hstack([dd_adj_list[i][:, :ind],
+                                            dd_adj_list[i][:,
+                                            ind + 1:]]).tocsr()
             # remove from d-p adj
             dp_adj = sp.vstack([dp_adj[:ind, :], dp_adj[ind + 1:, :]])
             # remove from drug additional features
             if mono:
-                drug_mono_adj = sp.vstack([drug_mono_adj[:ind, :], drug_mono_adj[ind + 1:, :]])
+                drug_mono_adj = sp.vstack(
+                    [drug_mono_adj[:ind, :], drug_mono_adj[ind + 1:, :]])
     print('remove finished')
     # ########################################
     # protein feature matrix
     # ########################################
-    protein_feat = sp.identity(protein_num)
+    # protein_feat = sp.identity(protein_num)
+    ind = t.LongTensor([range(protein_num), range(protein_num)])
+    val = t.FloatTensor([1] * protein_num)
+    protein_feat = t.sparse.FloatTensor(ind, val,
+                                        t.Size([protein_num, protein_num]))
 
     # ########################################
     # drug feature matrix
     # ########################################
-    drug_feat = sp.identity(drug_num - isolated_num)
+    row = np.array(range(drug_num), dtype=np.long)
+    col = np.array(range(drug_num), dtype=np.long)
+    # drug_feat = sp.identity(drug_num - isolated_num)
     if mono:
-        drug_feat = sp.hstack([drug_feat, drug_mono_adj])
+        # drug_feat = sp.hstack([drug_feat, drug_mono_adj])
+        adj = drug_mono_adj.tocoo()
+        row = np.append(row, adj.row)
+        col = np.append(col, adj.col + drug_num)
+    else:
+        mono_num = 0
+
+    ind = t.LongTensor([row, col])
+    val = t.FloatTensor([1] * len(row))
+
+    drug_feat = t.sparse.FloatTensor(ind, val, t.Size([drug_num, drug_num
+                                                       + mono_num]))
 
     # return a dict
     data = {'d_feat': drug_feat,
@@ -90,16 +116,8 @@ def load_data(path, dd_et_list, mono=True):
             'dd_adj_list': dd_adj_list,
             'dp_adj': dp_adj,
             'pp_adj': pp_adj}
-    return data
-
-
-def load_data_torch(path, dd_et_list, mono=True):
-    data = load_data(path, dd_et_list, mono=mono)
-    data['d_feat'] = t.tensor(data['d_feat'].toarray(), dtype=t.float32)
 
     n_et = len(dd_et_list)
-    n_drug = data['d_feat'].shape[0]
-    adj_list = data['dd_adj_list']
 
     num = [0]
     edge_index_list = []
@@ -109,7 +127,7 @@ def load_data_torch(path, dd_et_list, mono=True):
 
     for i in range(n_et):
         # pos samples
-        adj = adj_list[i].tocoo()
+        adj = dd_adj_list[i].tocoo()
         edge_index_list.append(t.tensor([adj.row, adj.col], dtype=t.long))
         edge_type_list.append(t.tensor([i] * adj.nnz, dtype=t.long))
         num.append(num[-1] + adj.nnz)
@@ -152,15 +170,3 @@ def cut_data(low, high, name, path='/Users/nyxfer/Docu/FM-PSEP/data/'):
         pickle.dump(ind, f)
     # for i in ind:
     #     print(dd_adj_list[i].nnz)
-
-
-
-
-# with open("/Users/nyxfer/Docu/FM-PSEP/data/training_samples_500.pkl", "rb") as f:
-#     et_list = pickle.load(f)
-et_list = list(range(1317))
-# data = load_data_torch("/Users/nyxfer/Docu/FM-PSEP/data/", et_list, mono=True)
-#
-# 
-
-
